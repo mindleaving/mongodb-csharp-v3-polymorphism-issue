81eeb39a7c5e13171a21eab4767f98e7760098e1
Jan <mindleaving@users.noreply.github.com>
2024-11-23 18:12:08 +0100

    Code that reproduces issue with polymorphism in MongoDB.Driver version 3.0.0
    Cannot query polymorphic types. Am I missing something?

---
+++ Models/Login.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbDriverV3PolymorphismBug.Models;
+
+[BsonKnownTypes(typeof(LocalLogin), typeof(ExternalLogin))]
+public abstract class Login
+{
+    public Guid Id { get; set; }
+}
+
+public class LocalLogin : Login
+{
+    public string Username { get; set; }
+}
+
+public class ExternalLogin : Login
+{
+    public string ExternalAccountId { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using MongoDbDriverV3PolymorphismBug.Models;
+
+BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
+var mongoDbSettings = new MongoClientSettings
+{
+    Credential = MongoCredential.CreateCredential("admin", "dev", File.ReadAllText(@"C:\temp\mongodb-dev-credential.txt"))
+};
+var mongoClient = new MongoClient(mongoDbSettings);
+var database = mongoClient.GetDatabase("MongoDbPolymorphismTest");
+var loginCollection = database.GetCollection<Login>(nameof(Login));
+
+var localLogin = new LocalLogin
+{
+    Id = Guid.NewGuid(),
+    Username = "test"
+};
+await loginCollection.InsertOneAsync(localLogin);
+var searchResult = await loginCollection
+    .OfType<LocalLogin>() // <--- Causes NotSupportedException "OfType is not supported with the configured discriminator convention"
+    .Find(x => x.Username == localLogin.Username)
+    .FirstOrDefaultAsync();
+
+if(searchResult != null)
+    Console.WriteLine("Successful roundtrip");
+else
+    Console.WriteLine("FAILED");
+
